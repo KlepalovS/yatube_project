fa85f98d8a1a59b0e7eb41caf281ea90dd7eb591
Вячеслав Клепалов <slavaklepalov@gmail.com>
2022-10-05 21:43:50 +0500

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Главная страница проекта')
+
+def group_posts(request):
+    return HttpResponse('Страница с группированными постами')
